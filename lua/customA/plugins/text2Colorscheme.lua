8db98955a2e455833e93ec23286b08644c72a3bc
idi8there <eyob01tesfaye@gmail.com>
2024-05-17 09:27:11 -0700

    added plugin [ T2Generate and a multitude of others ... ] to nvim config
    This is a plugin that genrates colorschemes based on prompt given to
    chatgpt. It doesnt current work since im on a free plan tho ... broke
    people problems :-(

--- lua/customA/plugins/text2Colorscheme.lua
+++ lua/customA/plugins/text2Colorscheme.lua
@@ -3,7 +3,7 @@ return {
         config = function ()
             require('text-to-colorscheme').setup {
                 ai = {
-                        openai_api_key = "sk-proj-Z4sl08XPsmLsjU1ihhrFT3BlbkFJ6IPGXMysJQIldBOXksYj",
+                        openai_api_key = "myopenapikey",
                         gpt_model = "gpt-3.5-turbo-16k-0613",
                     },
 
