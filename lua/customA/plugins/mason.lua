ca23783b8a596b9a5dd0ffb3477d316f77e5a5d3
idi8there <eyob01tesfaye@gmail.com>
2024-05-21 23:25:14 -0700

    Fixed Lsp not lauching post installation
    Lsp had issue not lauching which was fixed due to a block of code taken
    from chatGpt ... which i dont understand much clearly.
    I belive it has something to do with initalizing the lsp for the listed
    languages tho i dont get how that works

--- lua/customA/plugins/mason.lua
+++ lua/customA/plugins/mason.lua
@@ -1,24 +1,25 @@
 return {
     "williamboman/mason.nvim",
     dependencies = {
-                'williamboman/mason-lspconfig.nvim',
-                'mhartington/formatter.nvim',
-                'jose-elias-alvarez/null-ls.nvim',
-                'mfussenegger/nvim-dap',
-                'rcarriga/nvim-dap-ui',
-                'mfussenegger/nvim-lint',
-                'neovim/nvim-lspconfig',
-                },
-    config = function ()
+        'williamboman/mason-lspconfig.nvim',
+        'mhartington/formatter.nvim',
+        'jose-elias-alvarez/null-ls.nvim',
+        'mfussenegger/nvim-dap',
+        'rcarriga/nvim-dap-ui',
+        'mfussenegger/nvim-lint',
+        'neovim/nvim-lspconfig',
+    },
+    config = function()
+        -- Setup Mason
         require("mason").setup({
             ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
                 }
             },
-             ensure_installed = {
+            ensure_installed = {
                 'golangci-lint',
                 'bash-language-server',
                 'lua-language-server',
@@ -51,8 +52,31 @@ return {
                 'sqlls',
                 'terraformls',
                 'delve'
-  }
-})
+            }
+        })
+
+        -- Ensure language servers are set up
+        local lspconfig = require("lspconfig")
+
+        -- Configure individual language servers
+        lspconfig.tsserver.setup{}  -- TypeScript server setup
+        lspconfig.bashls.setup{}    -- Bash server setup
+        lspconfig.lua_ls.setup{} -- Lua server setup, ensure you have sumneko_lua or lua_ls
+        lspconfig.vimls.setup{}     -- Vim server setup
+        lspconfig.gopls.setup{}     -- Go server setup
+        lspconfig.yamlls.setup{}    -- YAML server setup
+        lspconfig.dockerls.setup{}  -- Docker server setup
+        lspconfig.sqlls.setup{}     -- SQL server setup
+        lspconfig.terraformls.setup{} -- Terraform server setup
+
+        -- Define additional autocmds if necessary, but avoid using invalid function calls
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "*",
+            callback = function()
+                -- Placeholder for a valid command or function
+                -- Example: vim.lsp.buf.formatting() or other valid LSP commands
+            end
+        })
     end,
 }
 
