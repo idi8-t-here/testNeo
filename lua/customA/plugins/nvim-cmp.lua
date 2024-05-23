71250c7e308a672aee3c88b76c3a694319bf0a4f
idi8there <eyob01tesfaye@gmail.com>
2024-05-23 03:35:01 -0700

    :sparkles:Added SuperTab to autocomplete:sparkles:
    the SuperTab works in a way such that upon the selection of a snippet
    the tab key is superloaded with command for the cursor to skip to the
    next relevant path in that snippet.
    The snippet and they're supertabing properties could be made customly
    aswell for custom languages :)

--- lua/customA/plugins/nvim-cmp.lua
+++ lua/customA/plugins/nvim-cmp.lua
@@ -31,6 +31,40 @@ return {
                 end,
             },
             mapping = {
+                ['<CR>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        if luasnip.expandable() then
+                            luasnip.expand()
+                        else
+                            cmp.confirm({
+                                select = true,
+                            })
+                        end
+                    else
+                        fallback()
+                    end
+                end),
+
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.locally_jumpable(1) then
+                        luasnip.jump(1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
                 ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-n>"] = cmp.mapping.scroll_docs(-4),
